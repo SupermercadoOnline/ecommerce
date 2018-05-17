c6f6493ae213bb84e5aa5034f852a42d2379053a
Thales Martins <thales.martins35@gmail.com>
2018-05-17 00:04:53 -0300

    Foram criados os métodos consultarPorId, consultarPorNome, insert, update e salvar na classe PessoasDAO.

--- src/model/PessoasDAO.php
+++ src/model/PessoasDAO.php
@@ -5,11 +5,21 @@ include_once 'PessoasBean.php';
 class PessoasDAO extends DAOInterface
 {
 
+    public function consultaPorId($id)
+    {
+        return $this->select("select * from pessoas where id = '$id' order by nome");
+    }
+
+    public function consultarPorNome($nome)
+    {
+        return $this->select("select * from pessoas where nome = '$nome' order by nome");
+    }
+
     protected function select($query): array
     {
         $listaDePessoas = array();
-        $pessoas = $this->mysqli->query($query);
-        while($row = $pessoas->fetch_array()) {
+        $select = $this->mysqli->query($query);
+        while($row = $select->fetch_array()) {
             $listaDePessoas[] = new PessoasBean($row['id'], $row['nome'],
                 $row['razaoSocial'], $row['cpf'], $row['cnpj'], $row['email'], $row['senha'],
                 $row['isAtivo'], $row['isReceberAlertasPromocao']);
@@ -20,20 +30,57 @@ class PessoasDAO extends DAOInterface
 
     protected function insert($bean)
     {
-        // TODO: Implement insert() method.
+        if($bean instanceof PessoasBean) {
+            $insert = $this->mysqli->prepare("insert into pessoas (nome, razao_social, cpf, 
+                cnpj, email, senha, is_receber_alertas_promocao)
+                values (?, ?, ?, ?, ?, ?, ?)");
+
+            $insert->bind_param("ssssssi", $bean->getNome(),
+                $bean->getRazaoSocial(), $bean->getCpf(), $bean->getCnpj(),
+                $bean->getEmail(), $bean->getSenha(), $bean->getIsReceberAlertasPromocao());
+
+            if($insert->execute()) {
+                $bean->setId($insert->insert_id);
+                return $bean;
+            }
+        }
+
+        return false;
     }
 
     protected function update($bean)
     {
-        // TODO: Implement update() method.
+        if($bean instanceof PessoasBean) {
+            $update = $this->mysqli->prepare("update pessoas set nome = ?, razao_social = ?,
+                cpf = ?, cnpj = ?, email = ?, senha = ?, is_receber_alertas_promocao = ?
+                where id = ?");
+
+            $update->bind_param("ssssssii",$bean->getNome(), $bean->getRazaoSocial(),
+                $bean->getCpf(), $bean->getCnpj(), $bean->getEmail(), $bean->getSenha(),
+                $bean->getIsReceberAlertasPromocao(), $bean->getId());
+
+            if($update->execute()) {
+                return true;
+            }
+
+            return false;
+        }
     }
 
-    protected function salvar($bean)
+    public function salvar($bean)
     {
-        // TODO: Implement salvar() method.
+        if($bean instanceof PessoasBean) {
+            if($this->consultaPorId($bean->getId()) != null) {
+                return $this->update($bean);
+            } else {
+                return $this->insert($bean);
+            }
+
+            return false;
+        }
     }
 
-    protected function delete($bean)
+    public function delete($bean)
     {
         // TODO: Implement delete() method.
     }
