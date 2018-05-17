0da46beda77c9c7fb5f15b8213bc88de35b2afc6
Murilo <murilo.jda@gmail.com>
2018-05-17 16:55:40 -0300

    Adicionado timezone America/Sao_Paulo como padrão

--- src/configs.php
+++ src/configs.php
@@ -1,4 +1,6 @@
 <?php
+date_default_timezone_set('America/Sao_Paulo');
+
 define('URL_HOST', 'http://192.168.99.100');
 define('URL_CSS_DIR', URL_HOST . '/css');
 define('URL_JS_DIR', URL_HOST . '/js');
