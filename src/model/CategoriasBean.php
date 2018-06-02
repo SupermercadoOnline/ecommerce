7c2f76e16a8aabc6cee0398be95f3927f02f6b1a
Lucas Fernando <lucasfernandog@gmail.com>
2018-06-02 00:14:27 -0300

    Criado os metodos na classe CategoriasDAO e finalizada a criação da classe CategoriasBean.

--- src/model/CategoriasBean.php
+++ src/model/CategoriasBean.php
@@ -8,5 +8,63 @@
 
 class CategoriasBean
 {
+    private $id;
+    private $nome;
+    private $isAtivo;
+
+    public function __construct($id, $nome, $isAtivo)
+    {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->isAtivo = $isAtivo;
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome): void
+    {
+        $this->nome = $nome;
+    }
+
+    public function getIsAtivo()
+    {
+        return $this->getIsAtivo();
+    }
+
+    /**
+     * @param $isAtivo
+     */
+    public function setIsAtivo($isAtivo): void
+    {
+
+        $this->$isAtivo = $isAtivo;
+
+    }
 
 }
--- src/model/CategoriasDAO.php
+++ src/model/CategoriasDAO.php
@@ -9,4 +9,58 @@
 class CategoriasDAO
 {
 
+
+    public function getAll(){
+        return $this->select('SELECT * FROM categoria_produtos ORDER BY id');
+    }
+
+
+    protected function select($query):array
+    {
+        $listaCategorias = array();
+        $selectListas = MySqlDAO::getResult($query);
+        while($row = $selectListas->fetch_array())
+            $listaCategorias[] = new CategoriasBean($row['id'], $row['nome'], $row['is_ativo']);
+
+        return $listaCategorias;
+    }
+
+
+    protected function insert($categoriasBean)
+    {
+        $query = "INSERT INTO categoria_produtos (id, nome, is_ativo) values (?,?,?)";
+        $parametros = array(
+            $categoriasBean->getId(),
+            $categoriasBean->getNome(),
+            $categoriasBean->getIsAtivo()
+
+        );
+        $result = MySqlDAO::executeQuery($query, $parametros);
+        if($result != false){
+
+            $categoriasBean->setId($result);
+            return $categoriasBean;
+
+        }
+
+
+
+    }
+
+    protected function update($bean)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function salvar($bean)
+    {
+        // TODO: Implement salvar() method.
+    }
+
+    public function delete($bean)
+    {
+        // TODO: Implement delete() method.
+    }
+
+
 }
