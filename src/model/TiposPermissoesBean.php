f437e68881e8b56d1e7be7fb9ae04dd2a8dcb844
Thales Martins <thales.martins35@gmail.com>
2018-06-03 01:28:31 -0300

    Foram adicionados as classes TiposPermissoesBean e DAO.

---
+++ src/model/TiposPermissoesBean.php
@@ -0,0 +1,50 @@
+<?php
+
+class TiposPermissoesBean
+{
+    private $id;
+    private $nome;
+
+    /**
+     * TiposPermissoesBean constructor.
+     * @param $id
+     * @param $nome
+     */
+    public function __construct($id, $nome)
+    {
+        $this->id = $id;
+        $this->nome = $nome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome): void
+    {
+        $this->nome = $nome;
+    }
+}
