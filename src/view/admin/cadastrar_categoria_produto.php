515fccb81d566dc456ab5c04c32db015f5bd8616
Lucas Fernando <lucasfernandog@gmail.com>
2018-06-04 20:47:57 -0300

    Concluida a classe para cadastro de categoria.

--- src/view/admin/cadastrar_categoria_produto.php
+++ src/view/admin/cadastrar_categoria_produto.php
@@ -10,8 +10,24 @@ if($_POST["Cadastrar"]){
     $categoriaProdutosDAO = new CategoriasProdutosDAO();
 
     if($categoriaProdutosDAO->salvar($categoriaProdutos)){
+        ?>
+        <div class="row">
+            <div class="alert alert-success">
+                Cadastrado com sucesso!
+            </div>
+        </div>
 
-    
+        <?php
+    }
+    else {
+        ?>
+        <div class="row">
+            <div class="alert alert-danger">
+                Erro ao cadastrar essa categoria!
+            </div>
+        </div>
+        <?php
+
 
     }
 
@@ -19,4 +35,36 @@ if($_POST["Cadastrar"]){
 
 }
 
+?>
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">
+                <b>Nova categoria</b>
+            </h3>
+        </div>
+        <div class="panel-body">
+            <form action="<?php echo URL_HOST ?>/admin/cadastrar_categoria_produto.php" method="post"></form>
+            <div class="row">
+                <div class="col-lg-4">
+                    <label for="nome">Nome:</label>
+                    <input id="nome" name="nome" type="text" class="form-control input-lg">
+                </div>
+
+
+
+            </div>
+
+
+
+
+
+
+
+
+
+        </div>
+    </div>
+
+
+<?php
 include_once 'footer.php';
