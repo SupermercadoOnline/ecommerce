02b6adc567d644dd2ec6906d161c86b7e2bd5cc7
Lucas Fernando <lucasfernandog@gmail.com>
2018-06-04 20:16:35 -0300

    Inicio da alteração no formulário html.

--- src/view/admin/cadastrar_categoria_produto.php
+++ src/view/admin/cadastrar_categoria_produto.php
@@ -1,5 +1,22 @@
 <?php
-include_once 'header.php';
 include_once '../../model/CategoriasProdutosDAO.php';
 include_once '../../model/CategoriasProdutosBean.php';
+include_once 'header.php';
 
+if($_POST["Cadastrar"]){
+
+    $categoriaProdutos = new CategoriasProdutosBean(null, $_POST["nome"], $_POST["isAtivo"]);
+
+    $categoriaProdutosDAO = new CategoriasProdutosDAO();
+
+    if($categoriaProdutosDAO->salvar($categoriaProdutos)){
+
+    
+
+    }
+
+
+
+}
+
+include_once 'footer.php';
