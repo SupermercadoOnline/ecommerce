dbf85e645777dc6b54ae319395fb45a5e2ee33e6
Lucas Fernando <lucasfernandog@gmail.com>
2018-06-05 19:59:22 -0300

    Inicio da implementação da classe de visualização de categorias.

--- src/view/admin/visualizar_categorias_produto.php
+++ src/view/admin/visualizar_categorias_produto.php
@@ -5,7 +5,8 @@ include_once "header.php";
 
 if($_GET["desativar"]){
     $id = $_GET["desativar"];
-
+    $categoriaDAO = new CategoriasProdutosDAO();
+    $categoria = $categoriaDAO->getProdutoPorId($id);
 
 
 
