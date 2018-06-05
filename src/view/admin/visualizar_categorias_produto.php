65dad4dffeb2057ea2ee3950c23c9947c46f6590
Lucas Fernando <lucasfernandog@gmail.com>
2018-06-04 21:40:36 -0300

    Criando classe para visualizar as categorias cadastradas.

---
+++ src/view/admin/visualizar_categorias_produto.php
@@ -0,0 +1,14 @@
+<?php
+include_once "../../model/CategoriasProdutosBean.php";
+include_once "../../model/CategoriasProdutosDAO.php";
+include_once "header.php";
+
+if($_GET["desativar"]){
+    $id = $_GET["desativar"];
+
+
+
+
+
+
+}
