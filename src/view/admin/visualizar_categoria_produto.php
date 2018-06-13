778b081566df364c9191d511e54113c482dad916
Lucas Fernando <lucasfernandog@gmail.com>
2018-06-13 12:13:21 -0300

    No if do botão desativar, foi adicionado o simbolo de negação para o empty.

--- src/view/admin/visualizar_categoria_produto.php
+++ src/view/admin/visualizar_categoria_produto.php
@@ -3,6 +3,8 @@ include_once "header.php";
 
 include_once ROOT_PATH . "/model/CategoriaProdutos.php";
 include_once ROOT_PATH . "/controller/CategoriaProdutosController.php";
+
+
 if(isset($_GET["retorno_edicao"])) {
     if (empty($_GET["retorno_edicao"])) {
         ?>
@@ -30,42 +32,42 @@ if(isset($_GET["retorno_edicao"])) {
 }
 
 
+    if (!empty($_GET["desativar"])) {
+        $id = $_GET["desativar"];
+        $categoriasController = new CategoriaProdutosController();
+        $categoria = $categoriasController->getById($id);
 
-if($_GET["desativar"]){
-    $id = $_GET["desativar"];
-    $categoriaDAO = new CategoriaProdutosController();
-    $categoria = $categoriaDAO->getById($id);
+        if ($categoria instanceof CategoriaProdutos) {
+            if ($categoriasController->delete($categoria)) {
+                ?>
 
-    if($categoria instanceof CategoriaProdutos){
-        if($categoriaDAO->delete($categoria)){
-            ?>
-
-            <div class="panel-body">
-                <div class="row">
-                    <div class="alert alert-success col-lg-4">
-                        Categoria inativado com sucesso!
-                        <button class="close" data-dismiss="alert">X</button>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="alert alert-success col-lg-4">
+                            Categoria inativado com sucesso!
+                            <button class="close" data-dismiss="alert">X</button>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <?php
-        } else {
-            ?>
+                <?php
+            } else {
+                ?>
 
-            <div class="panel-body">
-                <div class="row">
-                    <div class="alert alert-danger col-lg-4">
-                        Não foi possível inativar a categoria!
-                        <button class="close" data-dismiss="alert">X</button>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="alert alert-danger col-lg-4">
+                            Não foi possível inativar a categoria!
+                            <button class="close" data-dismiss="alert">X</button>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <?php
+                <?php
+            }
         }
     }
-}
+
 
 ?>
 
