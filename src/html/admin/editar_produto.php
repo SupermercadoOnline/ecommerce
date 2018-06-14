6fd5337d140ca5652daa696866d410feeeed7dc2
KESLEY <kesley0@hotmail.com>
2018-06-14 14:47:25 -0300

    direcionamento de mensagem para a tela de visualização de produtos

--- src/html/admin/editar_produto.php
+++ src/html/admin/editar_produto.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kesle
- * Date: 14/06/2018
- * Time: 13:48
- */
+include_once '../../controller/ProdutosController.php';
+
+if($_POST["alterar"]) {
+    $produto = new Produtos($_POST["id"], $_POST["nome"], $_POST["categoria"], $_POST["preco"],
+        $_POST["fabricante"], $_POST["descricao"], $_POST["estoque_minimo"], true);
+
+    $produtoDao = new ProdutosController();
+
+    if ($produtoDao->salvar($produto)) {
+
+        header("location: /admin/visualizar_produto.php?retorno_edicao=1");
+
+    } else {
+
+        header("location: /admin/visualizar_produto.php?retorno_edicao=0");
+
+    }
+
+
+}
