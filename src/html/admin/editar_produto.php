c3eb2bcfccbd4c7762f53348aeefe93a6484d7df
KESLEY <kesley0@hotmail.com>
2018-06-14 21:21:08 -0300

    Adicionado o tratamento com mascara para preços

--- src/html/admin/editar_produto.php
+++ src/html/admin/editar_produto.php
@@ -1,8 +1,9 @@
 <?php
 include_once '../../controller/ProdutosController.php';
+include_once '../../functions.php';
 
 if($_POST["alterar"]) {
-    $produto = new Produtos($_POST["id"], $_POST["nome"], $_POST["categoria"], $_POST["preco"],
+    $produto = new Produtos($_POST["id"], $_POST["nome"], $_POST["categoria"], remover_mascara_reais($_POST["preco"]),
         $_POST["fabricante"], $_POST["descricao"], $_POST["estoque_minimo"], true);
 
     $produtoDao = new ProdutosController();
